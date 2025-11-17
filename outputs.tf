b8551a5366780a3506797edf2866e9eaa5643d18
Jeff Sani <jeff.sani@gmail.com>
2025-11-17 15:39:57 -0800

    altered sensitivity and of token and id to false so they are visible

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "tunnel_token" {
   description = "The token for the created tunnel. Pass this to your cloudflared service."
   value       = data.cloudflare_zero_trust_tunnel_cloudflared_token.app_tunnel_token
-  sensitive   = true
+  sensitive   = false
 }
 
 output "tunnel_id" {
   description = "The ID of the created tunnel."
   value       = cloudflare_zero_trust_tunnel_cloudflared.app_tunnel
-  sensitive = true
+  sensitive = false
 }
 
 output "application_url" {

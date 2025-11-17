6bfbe2f3b5385008f18365fb56ae453f118a46b0
Jeff Sani <jeff.sani@gmail.com>
2025-11-17 15:52:04 -0800

    expose token and in in output

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,15 @@
+//Use terraform output -raw tunnel_token to get the token value without quotes from the cli
+//Use terraform output -raw tunnel_id to get the tunnel ID value without quotes from the cli
+
 output "tunnel_token" {
   description = "The token for the created tunnel. Pass this to your cloudflared service."
-  value       = data.cloudflare_zero_trust_tunnel_cloudflared_token.app_tunnel_token
+  value       = nonsensitive(data.cloudflare_zero_trust_tunnel_cloudflared_token.app_tunnel_token)
   sensitive   = true
 }
 
 output "tunnel_id" {
   description = "The ID of the created tunnel."
-  value       = cloudflare_zero_trust_tunnel_cloudflared.app_tunnel
+  value       = nonsensitive(cloudflare_zero_trust_tunnel_cloudflared.app_tunnel)
   sensitive = true
 }
 

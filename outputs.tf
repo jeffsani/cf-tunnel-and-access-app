0eb2c7b6f65b98945f531efc1e6c1e880302f023
Jeff Sani <jeff.sani@gmail.com>
2025-11-17 15:55:49 -0800

    try 3

--- outputs.tf
+++ outputs.tf
@@ -4,13 +4,13 @@
 output "tunnel_token" {
   description = "The token for the created tunnel. Pass this to your cloudflared service."
   value       = nonsensitive(data.cloudflare_zero_trust_tunnel_cloudflared_token.app_tunnel_token)
-  //sensitive   = true
+  sensitive   = true
 }
 
 output "tunnel_id" {
   description = "The ID of the created tunnel."
   value       = nonsensitive(cloudflare_zero_trust_tunnel_cloudflared.app_tunnel)
-  //sensitive = true
+  sensitive = true
 }
 
 output "application_url" {
